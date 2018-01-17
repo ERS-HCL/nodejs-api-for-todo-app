3b6d04bc6473227d7607383fbde59606638c6003
Arif Khan <tech.aarifkhan@gmail.com>
2018-01-17 13:44:07 +0530

    updated app

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-
+const PORT = 4000
 var bodyParser = require('body-parser');
 const app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://tutorials.way2programming.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -33,9 +30,8 @@ app.use('/api', routes);
 // Add headers
 
 const mongoose = require('mongoose');
-const Company = require('./models/company.model');
 
-const MONGO_DB_URI = 'mongodb://arifkhan08:khanarif08@ds135777.mlab.com:35777/arif123';
+const MONGO_DB_URI = 'mongodb://localhost/todo';
 
 var options = {
     "server": {
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const Router = express.Router();
-const companyRoutes = require('./company.route');
 const todoRoutes = require('./todo.route');
-Router.use('/companies', companyRoutes)
 Router.use('/todos', todoRoutes)
 module.exports = Router;
